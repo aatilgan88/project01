b4ca19169a40fa76e0196d23fe8f56148fc9ce76
Ali Atilgan <a.atilgan1720@gmail.com>
2021-02-28 20:57:29 -0600

    second commit from local master

--- src/project01/FirstClass.java
+++ src/project01/FirstClass.java
@@ -3,7 +3,8 @@ package project01;
 public class FirstClass {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("first commit from local master");
+		System.out.println("Second line of code that I am typing");
 
 	}
 
